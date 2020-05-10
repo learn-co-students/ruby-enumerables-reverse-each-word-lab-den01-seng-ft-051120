ebb33a3c6b017a310ac8f242ea61d9f98f37a545
Gerald Bivens <geraldbivens@gmail.com>
2020-05-10 23:30:57 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+def reverse_each_word(str)
+  arr = str.split
+  arr.each { |i| p i.reverse }
+  # .join the elements of the Array together into a String
+end
+
+# String to Array
+# Reverse each individual word in the Array, without changing the order of the words
+  # Do I need to get all of the Strings out of the Array to be able to reverse them? 
+  # This seems unlikely.
+# Array to String (.join ?), then return
+
+
